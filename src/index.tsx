deb7d4e32274f694a88f8592f5533eca90369c2c
Tomasz Durałek <tomek@blogweb.pl>
2022-10-24 19:13:13 +0200

    replace router to hash

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { store } from './app/store';
