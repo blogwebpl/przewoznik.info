170fd5cb4b02388c5f8a7b9934745e970f002e47
Tomasz Durałek <tomek@blogweb.pl>
2022-10-26 18:17:22 +0200

    fix url

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const env = process.env.NODE_ENV;
 
-const URL = env === 'development' ? 'http://localhost:3001/api' : `${window.location.href}api`;
+const URL = env === 'development' ? 'http://localhost:3001/api' : `${window.location.origin}/api`;
 
 const instance = axios.create({
 	baseURL: URL,
